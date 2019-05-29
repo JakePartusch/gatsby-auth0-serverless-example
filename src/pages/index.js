5e3a5cb3d4efe7c6055c9eef39649be89a8192a6
Jake Partusch <jakepartusch@gmail.com>
2019-05-28 21:01:15 -0500

    feat: handle auth on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import { login, isAuthenticated, getProfile } from "../utils/auth"
 
-export default () => <div>Hello world!</div>
+const Index = () => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
+
+  const user = getProfile()
+  console.log(user)
+
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
+
+export default Index
